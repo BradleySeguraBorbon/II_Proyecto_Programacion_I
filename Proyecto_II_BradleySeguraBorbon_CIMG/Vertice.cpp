f46825312c1b6abf8565737af6b880076ee4a25a
BradleySeguraBorbon <bradley.segura.borbon@est.una.ac.cr>
2023-10-13 21:42:22 -0600

    Added Vertice class

---
+++ Proyecto_II_BradleySeguraBorbon_CIMG/Vertice.cpp
@@ -0,0 +1,17 @@
+#include "Vertice.h"
+
+void Vertice::setX(float _x) {
+	x = _x;
+}
+
+void Vertice::setY(float _y) {
+	y = _y;
+}
+
+float Vertice::getX() {
+	return x;
+}
+
+float Vertice::getY() {
+	return y;
+}
---
+++ Proyecto_II_BradleySeguraBorbon_CIMG/Vertice.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Vertice {
+	float x, y;
+
+public:
+	Vertice() : x(0), y(0) {}
+	Vertice(float _x, float _y) : x(_x), y(_y) {}
+	~Vertice() {}
+	void setX(float _x);
+	void setY(float _y);
+	float getX();
+	float getY();
+};
